8aab97905641688b8bcc8ec3a3d8326d1b0f602b
Florent Hivert <Florent.Hivert@lri.fr>
2020-01-13 18:48:20 +0100

    Fixed include path + HPCombi config

--- libsemigroups_cppyy/__init__.py
+++ libsemigroups_cppyy/__init__.py
@@ -12,9 +12,7 @@ via cppyy:
 
 For this to work, libsemigroups must be installed on your computer (i.e. the
 executables libsemigroups.0.dylib, libsemigroups.dylib, and libsemigroups.a
-must be somewhere on your computer where cppyy can load it). libsemigroups
-should have been compiled with HPCombi support disabled via the configuration
-flag --disable-hpcombi.
+must be somewhere on your computer where cppyy can load it).
 """
 
 import cppyy
@@ -23,12 +21,13 @@ from cppyy.gbl import std
 import libsemigroups_cppyy.detail
 from libsemigroups_cppyy.adapters import *
 
-cppyy.add_include_path("/usr/local/include/fmt")
+cppyy.add_include_path("/usr/local/include/libsemigroups/fmt")
 cppyy.add_include_path("/usr/local/include/libsemigroups")
 
 cppyy.load_library("libsemigroups")
 
 cppyy.cppdef("#define FMT_HEADER_ONLY")
+cppyy.cppdef("#define HPCOMBI_CONSTEXPR_FUN_ARGS")
 
 cppyy.include("action.hpp")
 cppyy.include("bmat8.hpp")
