a20d3817f1d29a80ac1ccb1cf8dec2497c459311
Florent Hivert <Florent.Hivert@lri.fr>
2020-01-17 12:09:27 +0100

    Fixed a test incompat No/HPCombi

--- tests/test_perm.py
+++ tests/test_perm.py
@@ -6,8 +6,9 @@ from libsemigroups_cppyy import Permutation
 
 class TestPerm(unittest.TestCase):
     def test_init(self):
+        Permutation([])
+        Permutation([0, 1])
         Permutation([0, 1, 2, 3])
-        self.assertEqual(Permutation([]), Permutation([0, 1]))
     def test_one(self):
         self.assertEqual(Permutation([1, 0, 2]) ** 2, Permutation([0, 1, 2]))
         self.assertEqual(Permutation([1, 0, 2]).ran(), [1, 0, 2])
