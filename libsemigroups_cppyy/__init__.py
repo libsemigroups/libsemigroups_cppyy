f14e65d7ea9d02fefcf9a4416116d18c0241f85b
Murray Whyte <mw231@st-andrews.ac.uk>
2020-02-12 17:01:01 +0000

    Fix problem with path in init file for running tests

--- libsemigroups_cppyy/__init__.py
+++ libsemigroups_cppyy/__init__.py
@@ -11,17 +11,25 @@ via cppyy:
     https://bitbucket.org/wlav/cppyy/
 
 For this to work, libsemigroups must be installed on your computer (i.e. the
-executables libsemigroups.0.dylib, libsemigroups.dylib, and libsemigroups.a
+executables libsemigroups.1.dylib, libsemigroups.dylib, and libsemigroups.a
 must be somewhere on your computer where cppyy can load it).
 """
 
 import cppyy
+import os
 from cppyy.gbl import std
+
+path = os.environ['PATH'].split(':')
+for d in path:
+  if d.find('include') != -1:
+    try:
+      cppyy.add_include_path(d)
+    except:
+        pass
 
 import libsemigroups_cppyy.detail
 from libsemigroups_cppyy.adapters import *
 
-cppyy.add_include_path("/usr/local/include/")
 cppyy.load_library("libsemigroups")
 
 cppyy.cppdef("#define FMT_HEADER_ONLY")
